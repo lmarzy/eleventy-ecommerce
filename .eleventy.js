7ef387c05f3480fd101de85abdb2af77840dd4ac
Lee Marsden <marzy@lees-mbp.home>
2021-02-18 10:49:18 +0000

    Initial commit

---
+++ .eleventy.js
@@ -0,0 +1,11 @@
+module.exports = config => {
+  return {
+    dir: {
+      input: 'src',
+      output: 'dist'
+    },
+    markdownTemplateEngine: 'njk',
+    dataTemplateEngine: 'njk',
+    htmlTemplateEngine: 'njk',
+  };
+};
---
+++ src/index.md
@@ -0,0 +1,8 @@
+---
+title: 'Hello, world'
+layout: 'layouts/main.html'
+---
+
+<article>
+  <h1>{{ title }}</h1>
+</article>
